f732e24db89aebcddcdeb558c92821331d1fe9e5
richelbilderbeek <rjcbilderbeek@gmail.com>
2024-12-22 09:22:20 +0100

    Use 'app.init_asset::<bevy::image::Image>();'

--- src/app.rs
+++ src/app.rs
@@ -17,7 +17,7 @@ pub fn create_app() -> App {
     if cfg!(test) {
         app.add_plugins(TaskPoolPlugin::default());
         app.add_plugins(AssetPlugin::default());
-        app.init_asset::<bevy::render::texture::Image>();
+        app.init_asset::<bevy::image::Image>();
     }
     app.add_systems(Startup, add_player);
 
